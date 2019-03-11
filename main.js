2cf64d33596989e254e488a275869f69a0ad3b2d
shamiulASC4 <shamiulislam1415@gmail.com>
2019-03-10 20:27:15 -0400

    Add files via upload
    Files for website.

---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+Alert message for contact information. 
+*/ 
+function message() {
+    if (confirm("Would you like to receive contact information?")) {
+        var text = "Email: shamiulislam616@gmail.com,";
+        var text1 = "Phone: 3477201663, "
+        var text2 = "Instagram: @anonymous.sham";
+        document.write(text, "\n" , text1, "\n",  text2);   
+    } 
+
+    
+    
+      
+}
+
+
